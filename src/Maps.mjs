53ea15c1c240950a09b90f55a2a34db8c4c5b6a8
Erik Itter <erik.itter@gmail.com>
2026-01-08 22:07:13 +0100

    Setup

---
+++ src/Maps.mjs
@@ -0,0 +1,8 @@
+
+let m1 = new Map();
+
+m1.set("Gial", { name: 'Gial Engstrand', clas: 'Wizard', level: 3 });
+m1.set("Lynyxia", { name: 'Lynyxia ?', clas: 'Hunter', lelve: 2 });
+
+console.log(m1);
+console.log(m1.get("Gial"));
