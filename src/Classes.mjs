fee09c3e01894ef4da680d0384d076c841cf7a49
Erik Itter <erik.itter@gmail.com>
2026-01-10 12:36:43 +0100

    Setup

---
+++ src/Classes.mjs
@@ -0,0 +1,37 @@
+
+class Character {
+    constructor(args) {
+        this.name = args.name ? args.name : 'unknown';
+        this.sex = args.sex ? args.sex : 'unknown';
+    }
+}
+
+class Wizard extends Character {
+    constructor(args) {
+        super(args);
+
+        this.spellcastingTrait = 'Knowledge';
+    }
+
+    toString() {
+        return 'Puny Wizard ' + this.name + ' (' + this.sex + ')';
+    }
+}
+
+class Warrior extends Character {
+    constructor(args) {
+        super(args);
+    }
+
+    toString() {
+        return `Mighty Warrior ${this.name} (${this.sex})`;
+    }
+}
+
+
+let party = [];
+party.push(new Wizard({ name: 'Gial Engstrand', sex: 'male' }));
+party.push(new Warrior({ name: 'Talokai', sex: 'female' }));
+
+
+party.forEach(c => { console.log(c.toString()) });
