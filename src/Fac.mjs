9e6986307648cbd2f0d592a1ccea9aec34b3ab1e
Erik Itter <erik.itter@gmail.com>
2026-01-12 19:42:14 +0100

    ...

--- src/Fac.mjs
+++ src/Fac.mjs
@@ -7,7 +7,12 @@
  * @returns 
  */
 function factorial(n) {
-    return (n != 1) ? n * factorial(n - 1) : 1;
+
+    if (n != 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
 }
 
 console.log(factorial(100));
