49d84cfb241c8a44bdcb1275c9b6f3fb2c06d02a
Erik Itter <erik.itter@gmail.com>
2026-01-18 21:48:02 +0100

    alternative loop

--- src/Arrays.mjs
+++ src/Arrays.mjs
@@ -23,4 +23,15 @@ for (const e of a1) {
     histogram[e - 1]++;
 }
 
+
+let histogram2 = new Array(24);
+for (let i = 0; i < histogram2.length; i++) {
+    histogram2[i] = 0;
+}
+
+for (let i = 0; i < a1.length; i++) {
+    histogram2[a1[i] - 1]++;
+}
+
 console.log(histogram);
+console.log(histogram2);
