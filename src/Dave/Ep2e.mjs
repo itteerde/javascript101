4bfcb4066b1909fc1d9ca7427f73378d9cbd31c0
David Milman <dave.mil.mail@gmail.com>
2026-02-01 16:41:02 -0500

    confused

--- src/Dave/Ep2e.mjs
+++ src/Dave/Ep2e.mjs
@@ -3,7 +3,7 @@ import { LibEp2e } from '../FVTT/Ep2e.mjs';
 let result = LibEp2e.randomInteger(0, 99);
 
 if (LibEp2e.isCritical(result)) {
-    console.log(`${result} is pritical.`);
+    console.log(`${result} is critical.`);
 } else {
     console.log(`${result} is not critical.`);
 }
