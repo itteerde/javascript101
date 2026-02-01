130b9484d40da37cd2face88077036c56af9f8b4
Erik Itter <erik.itter@gmail.com>
2026-02-01 22:30:41 +0100

    ...

--- src/Dave/Ep2e.mjs
+++ src/Dave/Ep2e.mjs
@@ -1,7 +1,9 @@
 import { LibEp2e } from '../FVTT/Ep2e.mjs';
 
-let result = LibEp2e.randomInteger(0, 99)
+let result = LibEp2e.randomInteger(0, 99);
 
 if (LibEp2e.isCritical(result)) {
-    console.log(result)
-} 
+    console.log(`${result} is pritical.`);
+} else {
+    console.log(`${result} is not critical.`);
+}
