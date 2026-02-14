ec0f9a65efdff24b67cec6f8842a93fa0cf093ce
Erik Itter <erik.itter@gmail.com>
2026-02-14 20:33:16 +0100

    Level DB

--- src/LevelDB.mjs
+++ src/LevelDB.mjs
@@ -3,9 +3,6 @@ import { Level } from 'level';
 const db = new Level('./data/armors', { valueEncoding: 'json' });
 
 async function browseDatabase() {
-    console.log('--- Database Contents ---');
-
-    // This will iterate through every key-value pair in alphabetical order
     for await (const [key, value] of db.iterator()) {
         console.log({ key: key, value: value });
     }
