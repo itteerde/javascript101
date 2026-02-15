e12c4d81f3b8052d0dc5a940a23ef7ab66aba40d
Erik Itter <erik.itter@gmail.com>
2026-02-15 20:50:59 +0100

    ?

--- src/Dave/barebones.mjs
+++ src/Dave/barebones.mjs
@@ -6,7 +6,7 @@ if (canvas.tokens.controlled.length !== 1) {
 let token = canvas.tokens.controlled[0];
 let hasBareBones = canvas.tokens.controlled[0].actor.items.filter(item => item.name === 'Bare Bones' && item.type === 'armor');
 //console.log(hasBareBones)
-hasBareBones.forEach(bareBones => {
+hasBareBones.forEach(async (bareBones) => {
     await bareBones.update({ tier: 2, baseThresholds: { major: 11, severe: 24 } })
 });
 
