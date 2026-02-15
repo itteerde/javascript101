e81d011f6151a1d7065c69ef80a150bd9e9d393f
David Milman <dave.mil.mail@gmail.com>
2026-02-15 14:49:02 -0500

    barebones.mjs practice

---
+++ src/Dave/barebones.mjs
@@ -0,0 +1,12 @@
+if (canvas.tokens.controlled.length !== 1) {
+    ui.notifications.warn("This macro only works when exactly a single token is selected.", { permanent: true });
+    return;
+}
+
+let token = canvas.tokens.controlled[0];
+let hasBareBones = canvas.tokens.controlled[0].actor.items.filter(item => item.name === 'Bare Bones' && item.type === 'armor');
+//console.log(hasBareBones)
+hasBareBones.forEach(bareBones => {
+    await bareBones.update({ tier: 2, baseThresholds: { major: 11, severe: 24 } })
+});
+
