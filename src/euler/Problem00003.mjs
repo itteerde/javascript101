17b6ba12a51e05083ecd57fb3bf0dd7d52492815
David Milman <dave.mil.mail@gmail.com>
2026-02-22 14:37:32 -0500

    practice

--- src/euler/Problem00003.mjs
+++ src/euler/Problem00003.mjs
@@ -5,7 +5,24 @@ import { CheckSolution } from "./CheckSolution.mjs";
  * 
  * What is the largest prime factor of the number 600851475143?
  */
+let n = Math.floor(Math.sqrt(600851475143))
 
-let solution = undefined; // put the solution (answer) here.
+for (n; n > 0; n--) {
+    if (600851475143 % n === 0) {
+        //let potentialPrime = n;
+        let isPrime = true;
+        for (let i = Math.floor(Math.sqrt(n)); i > 1; i--) {
+            if (n % i === 0) {
+                isPrime = false;
+                break
+            }
+        }
+        if (isPrime === true) {
+            break
+        }
+    }
+}
 
+console.log(n)
+let solution = n; // put the solution (answer) here.
 console.log(CheckSolution.check(3, solution));
