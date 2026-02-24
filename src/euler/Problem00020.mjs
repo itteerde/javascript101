8455680440904204c6897ed706000df87f2581c6
Erik Itter <erik.itter@gmail.com>
2026-02-24 22:19:01 +0100

    ...

---
+++ src/euler/Problem00020.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * Factorial Digit Sum
+ * 
+ * This is quite simple using the built in BigInt.
+ * 
+ * https://projecteuler.net/problem=20
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(20, solution));
