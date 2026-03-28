9590baf87ba8f56e67b45951662e8571ba39a92a
Erik Itter <erik.itter@gmail.com>
2026-03-28 02:57:57 +0100

    ...

---
+++ src/dom/006/index.js
@@ -0,0 +1,26 @@
+const source = document.querySelector("#draggable");
+const target = document.querySelector("#drop-target");
+
+function dragstartHandler(e) {
+    // Add different types of drag data
+    e.dataTransfer.setData("text/plain", e.target.innerText);
+    e.dataTransfer.setData("text/html", e.target.outerHTML);
+    e.dataTransfer.setData(
+        "text/uri-list",
+        e.target.ownerDocument.location.href,
+    );
+}
+
+source.addEventListener("dragstart", dragstartHandler);
+
+target.addEventListener("dragover", (e) => {
+    //console.log({ target: target, event: e });
+    e.preventDefault();
+});
+
+target.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text/plain");
+    console.log({ target: target, event: e, data: data });
+    target.innerHTML += data;
+});
