32c0c708c40a836b3e47642764c74975193dacfe
Erik Itter <erik.itter@gmail.com>
2026-03-28 22:54:27 +0100

    added euler problem 12

---
+++ src/euler/Problem00012.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * Highly Divisible Triangular Number
+ * 
+ * Full brute force will not finish in time. Think about when to stop checking. Apparently Triangle Numbers cannot be Square Numbers?
+ * 
+ * https://projecteuler.net/problem=12
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(12, solution));
