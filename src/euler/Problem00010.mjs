d25d6b770c135f3355f665d227825a42688167c5
Erik Itter <erik.itter@gmail.com>
2026-03-28 22:00:44 +0100

    added euler problem 10

---
+++ src/euler/Problem00010.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * Summation of Primes
+ * 
+ * Naively this is easy, but is it still fast enough?. Damn those computers, just need the very first insight to make if (very much) fast enough. No sieve required.
+ * 
+ * https://projecteuler.net/problem=10
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(10, solution));
