c0d0a740e6ddf2dda4808c73019f8d6efbae9725
Erik Itter <erik.itter@gmail.com>
2026-03-28 21:42:13 +0100

    added euler problem 7

---
+++ src/euler/Problem00007.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * 10 001st Prime
+ * 
+ * Naively this is easy, but how long will it run? Actually way to short to make the point that one should implement the sieve. Damn computers are to fast.
+ * 
+ * https://projecteuler.net/problem=7
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(7, solution));
