8b67421b785c470915eff3ed703c35575e8a2b84
Erik Itter <erik.itter@gmail.com>
2026-03-28 21:51:09 +0100

    added euler problem 9

---
+++ src/euler/Problem00009.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * Special Pythagorean Triplet
+ * 
+ * Naively this is easy, and there is no way that takes long, so easy.
+ * 
+ * https://projecteuler.net/problem=9
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(9, solution));
