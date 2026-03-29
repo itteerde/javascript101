7d10b0f03fc95a210daa68fb1b295e176e473a93
Erik Itter <erik.itter@gmail.com>
2026-03-29 04:16:50 +0200

    added euler problem 25

---
+++ src/euler/Problem00025.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * 1000-digit Fibonacci Number
+ * 
+ * BigInt avoids implementing adding with carry on strings representing integers -- and makes it really easy.
+ * 
+ * https://projecteuler.net/problem=25
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(25, solution));
