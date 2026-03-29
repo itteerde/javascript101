eb1ea0b734275cd3b1aaf36d839946043aeef4f2
Erik Itter <erik.itter@gmail.com>
2026-03-29 04:02:28 +0200

    added euler problem 29

---
+++ src/euler/Problem00019.mjs
@@ -0,0 +1,13 @@
+import { CheckSolution } from "./CheckSolution.mjs";
+
+/**
+ * Counting Sundays
+ * 
+ * Date does actually work with negative numbers for dates prior to 1970, at least for four digit years, so it becomes easy(ish). Good exercise to explore Date, otherwise useless.
+ * 
+ * https://projecteuler.net/problem=19
+ */
+
+let solution = undefined; // put the solution (answer) here.
+
+console.log(CheckSolution.check(19, solution));
