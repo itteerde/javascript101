71cac0f5f20491bc8875ef96b0b4eda497c1218c
David Milman <dave.mil.mail@gmail.com>
2026-03-30 12:52:10 -0400

    euler problem 6.mjs

--- src/euler/Problem00006.mjs
+++ src/euler/Problem00006.mjs
@@ -8,6 +8,16 @@ import { CheckSolution } from "./CheckSolution.mjs";
  * https://projecteuler.net/problem=6
  */
 
-let solution = undefined; // put the solution (answer) here.
+let sumOne = 0;
+let sumTwo = 0;
+
+for (let n = 1; n <= 100; n++) {
+    sumOne = sumOne + n;
+    sumTwo = sumTwo + Math.pow(n, 2);
+}
+
+answer = Math.pow(sumOne, 2) - sumTwo;
+
+let solution = answer; // put the solution (answer) here.
 
 console.log(CheckSolution.check(6, solution));
